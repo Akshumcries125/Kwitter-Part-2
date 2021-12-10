2653920b20152ca66c01767f8bb4e027e6b685b8
Akshumcries125 <83885762+Akshumcries125@users.noreply.github.com>
2021-12-10 17:27:31 -0600

    Add files via upload

---
+++ kwiiter_room.js
@@ -0,0 +1,12 @@
+ var firebaseConfig = {
+    apiKey: "AIzaSyCsAxYkJ5MTnryBa4MQfZYXY5mv-TGiUIE",
+    authDomain: "kwitter-project-f7b49.firebaseapp.com",
+    databaseURL: "https://kwitter-project-f7b49-default-rtdb.firebaseio.com",
+    projectId: "kwitter-project-f7b49",
+    storageBucket: "kwitter-project-f7b49.appspot.com",
+    messagingSenderId: "371961483723",
+    appId: "1:371961483723:web:b5bb12d7efa01f2c03e64e"
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
